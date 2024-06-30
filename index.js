45a25658b5dc9d5b6b49df98361c9a04d5864b08
Vitor Latorraca <vitorlatorraca5@gmail.com>
2024-06-30 13:50:19 -0400

    new

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const server = express();
 
 // localhost:3000/curso
 server.get('/curso',  (req, res) => {
-    console.log('ACESSOU A ROTA');
+    return res.json({curso: 'Node.js'});
 })
 
-server.listen(3000);
+server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
